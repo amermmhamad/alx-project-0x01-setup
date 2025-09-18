630550a76e5f47cd238f274b98d3d66a61779e23
Amer <amermmhamad@gmail.com>
2025-09-19 02:09:30 +0300

    Added Link

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,1 +1,14 @@
-// Placeholder
+import Header from "@/components/layout/Header";
+
+const Posts: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Posts Page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Posts;
